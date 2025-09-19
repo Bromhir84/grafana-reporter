f80d9df015da99adaf32d1623f4fba9c854fd8a3
Roel Keller <r.keller@mdcs.nl>
2025-09-19 12:43:00 +0200

    revert to former api script

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ echo "Dependencies installed."
 # Ensure local pip bin is in PATH
 export PATH="$HOME/.local/bin:$PATH"
 echo "Starting server..."
-cd /app
+cd /app/repo
 
 # Start uvicorn as the main process
-exec uvicorn repo.main:app --host 0.0.0.0 --port 8000
+exec uvicorn report_api:app --host 0.0.0.0 --port 8000
